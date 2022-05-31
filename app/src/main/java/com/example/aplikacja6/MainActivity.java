f7c9127f22e2a317cf4ecfc8b666e0d49bfbe5f8
Student <huntergerald00@gmail.com>
2022-05-31 15:44:05 +0200

    druggi2

--- app/src/main/java/com/example/aplikacja6/MainActivity.java
+++ app/src/main/java/com/example/aplikacja6/MainActivity.java
@@ -1,14 +1,46 @@
 package com.example.aplikacja6;
 
+//https://stackoverflow.com/questions/12763104/move-a-ball-with-accelerometer-android
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager sensorManager;
+    private Sensor acceleometer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        acceleometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        Sensor s = sensorEvent.sensor;
+        if (s.getType() == Sensor.TYPE_ACCELEROMETER) {
+
+            Log.i("pozycja X:", String.valueOf(sensorEvent.values[0]));
+            Log.i("pozycja Y:", String.valueOf(sensorEvent.values[1]));
+            Log.i("pozycja Z:", String.valueOf(sensorEvent.values[2]));
+        }
+
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
     }
 }
