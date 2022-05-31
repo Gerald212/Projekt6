c173b553ade4d910b8707f1ea15cd011f3f3e98c
Student <huntergerald00@gmail.com>
2022-05-31 16:08:20 +0200

    4

--- app/src/main/java/com/example/aplikacja6/MainActivity.java
+++ app/src/main/java/com/example/aplikacja6/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.aplikacja6;
 
 //https://stackoverflow.com/questions/12763104/move-a-ball-with-accelerometer-android
+//https://stackoverflow.com/questions/6479637/android-accelerometer-moving-ball
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,6 +12,7 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Display;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
@@ -20,17 +22,29 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     private ImageView ball;
 
-    int xa=0;
-    int ya=0;
+    int xa=-50;
+    int ya=-50;
+
+    int xmax;
+    int ymax;
+    Display display;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        display  = getWindowManager().getDefaultDisplay();
+        xmax = display.getWidth();
+        ymax = display.getHeight();
+
         ball = (ImageView) findViewById(R.id.ball);
         ball.setImageResource(R.drawable.ball);
-        ball.scrollTo(xa, ya);
+        //ball.setMaxHeight(50);
+        //ball.setMaxWidth(50);
+        ball.setX(xa);
+        ball.setY(ya);
+        //ball.scrollTo(xa, ya);
 
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         acceleometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
